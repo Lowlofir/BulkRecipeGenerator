6d9be3492d6e41ff6f5b5f09f0318c4e3e204390
IWTKU <a.svizhenko@gmail.com>
2019-05-30 10:41:52 +0300

    default settings changed

--- Source/Ad2.cs
+++ Source/Ad2.cs
@@ -21,13 +21,13 @@ namespace Ad2mod
     public class Ad2Settings : ModSettings
     {
         public int defaultThreshold = 60;
-        public bool limitToX5 = true;
+        public bool limitToX5 = false;
         public bool useRightClickMenu = true;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref defaultThreshold, "defaultThreshold", 60);
-            Scribe_Values.Look(ref limitToX5, "limitToX5", true);
+            Scribe_Values.Look(ref limitToX5, "limitToX5", false);
             Scribe_Values.Look(ref useRightClickMenu, "useRightClickMenu", true);
             base.ExposeData();
         }
